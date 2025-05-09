2cec3cc896b58369931929ba225a995576b80688
lucas juarez <lujuarez@emeal.nttdata.com>
2025-05-09 17:27:30 -0300

    add movies slide show widget

---
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../../../domain/entitties/movie.dart';
+
+class MoviesSlideshow extends StatelessWidget {
+  final List<Movie> movies;
+
+  const MoviesSlideshow({super.key, required this.movies});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
