d92e5e1dd4d170a06926b1cfcabbf09274518888
lucas juarez <lujuarez@emeal.nttdata.com>
2025-05-12 14:36:45 -0300

    add new widget for movie horizontal list view

---
+++ lib/presentation/widgets/movies/movies_horizontal_listview.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+class MoviesHorizontalListview extends StatelessWidget {
+  final List<Movie> movies;
+  final String? title;
+  final String? subTitle;
+  final VoidCallback? loadNextPage;
+
+  const MoviesHorizontalListview({
+    super.key,
+    required this.movies,
+    this.title,
+    this.subTitle,
+    this.loadNextPage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
