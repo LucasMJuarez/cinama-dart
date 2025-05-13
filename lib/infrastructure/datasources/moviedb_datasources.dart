f08671b1aafdb96f25cfa191bcef8a32207c6a6a
lucas juarez <lujuarez@emeal.nttdata.com>
2025-05-13 18:04:25 -0300

    added infra and domain for upcoming and top-rated movies

--- lib/infrastructure/datasources/moviedb_datasources.dart
+++ lib/infrastructure/datasources/moviedb_datasources.dart
@@ -37,15 +37,33 @@ class MoviedbDatasources extends MoviesDatasource {
     return _jsonToMovie(response.data);
   }
 
-  List<Movie> _jsonToMovie(Map<String, dynamic> json) {
-    final movieDBResponse = MovieDbResponse.fromJson(json);
-
-    final List<Movie> movies =
-        movieDBResponse.results
-            .where((moviedb) => moviedb.posterPath != 'no-poster')
-            .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
-            .toList();
-
-    return movies;
+  @override
+  Future<List<Movie>> getTopRated({int page = 1}) async {
+    final response = await dio.get(
+      '/movie/top_rated',
+      queryParameters: {'page': page},
+    );
+    return _jsonToMovie(response.data);
   }
+
+  @override
+  Future<List<Movie>> getUpcoming({int page = 1}) async {
+    final response = await dio.get(
+      '/movie/upcoming',
+      queryParameters: {'page': page},
+    );
+    return _jsonToMovie(response.data);
+  }
+}
+
+List<Movie> _jsonToMovie(Map<String, dynamic> json) {
+  final movieDBResponse = MovieDbResponse.fromJson(json);
+
+  final List<Movie> movies =
+      movieDBResponse.results
+          .where((moviedb) => moviedb.posterPath != 'no-poster')
+          .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+          .toList();
+
+  return movies;
 }
